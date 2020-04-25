4e97ac4525adeec271ebf0c4a8ab7cdd824494bd
Rachel Vogus <rvogus@gmail.com>
2020-04-25 12:08:24 -0400

    Second Project First Commit

---
+++ src/build/precache-manifest.55fd14e936bf4009ff252c26da5b9fbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a56df62f9787f4db66f272624705c736",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e0389d233d28d279a98",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "0cf6d6148858952f572d",
+    "url": "/static/js/2.f2b8de2a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f2b8de2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e0389d233d28d279a98",
+    "url": "/static/js/main.a6f04150.chunk.js"
+  },
+  {
+    "revision": "c6cd80a6fce542ddaee9",
+    "url": "/static/js/runtime-main.b9244b71.js"
+  }
+]);
